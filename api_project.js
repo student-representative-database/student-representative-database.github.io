89f68987702369667eca4cca46ce1cb9b4bb353b
AndrasBalla <ab223sq@student.lnu.se>
2017-05-29 14:35:48 +0200

    Api update

--- api_project.js
+++ api_project.js
@@ -1,13 +1,14 @@
 define({
-  "name": "",
-  "version": "0.0.0",
-  "description": "",
+  "url": "/api/v1/",
+  "name": "example",
+  "version": "0.1.0",
+  "description": "apiDoc basic example",
+  "apidoc": "0.3.0",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-05-19T17:26:45.128Z",
+    "time": "2017-05-29T12:35:09.199Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
